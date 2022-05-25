b96657ed8f5f96f947491208aa8f938b38f33268
brunoleo223 <brunoleo223@gmail.com>
2022-05-25 19:54:41 -0300

    volume configuration

--- components/Player/index.js
+++ components/Player/index.js
@@ -1,9 +1,11 @@
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../../atoms/songAtom";
 import useSpotify from "../../hooks/useSpotify";
 import useSongInfo from "../../hooks/useSongInfo";
+import { FastForwardIcon, PauseIcon, PlayIcon, ReplyIcon, RewindIcon, SwitchHorizontalIcon, VolumeUpIcon, VolumeOffIcon } from "@heroicons/react/solid";
+import { debounce } from 'lodash';
 
 function Player() {
     const spotifyApi = useSpotify();
@@ -27,12 +29,36 @@ function Player() {
         }
     }
 
+    const handlePlayPause = () => {
+        spotifyApi.getMyCurrentPlaybackState().then((data) => {
+            if(data.body.is_playing){
+                spotifyApi.pause();
+                setIsPlaying(false);
+            } else {
+                spotifyApi.play();
+                setIsPlaying(true);
+            }
+        })
+    }
+
     useEffect(() => {
         if(spotifyApi.getAccessToken() && !currentTrackId){
             fetchCurrenSong();
             setVolume(50);
         }
     }, [currentTrackId, spotifyApi, session])
+
+    useEffect(() => {
+        if(volume > 0 && volume < 100){
+            debouncedAdjustVolume(volume);
+        }
+    }, [volume])
+
+    const debouncedAdjustVolume = useCallback(
+        debounce((volume) => {
+            spotifyApi.setVolume(volume).catch((err) => {});
+        }, 500, [])
+    )
 
     return (
         <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
@@ -42,6 +68,31 @@ function Player() {
                     <h3>{songInfo?.name}</h3>
                     <p>{songInfo?.artists?.[0]?.name}</p>
                 </div>
+            </div>
+
+            <div className="flex items-center justify-evenly">
+                <SwitchHorizontalIcon className="button" />
+                <RewindIcon className="button" />
+                {isPlaying ? (
+                    <PauseIcon onClick={handlePlayPause} className="button w-10 h-10" />
+                ) : (
+                    <PlayIcon onClick={handlePlayPause} className="button w-10 h-10" />
+                )}
+                <FastForwardIcon className="button" />
+                <ReplyIcon className="button" />
+            </div>
+
+            <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
+                <VolumeOffIcon onClick={() => volume > 0 && setVolume(volume - 10)} className="button" />
+                <input 
+                    type="range" 
+                    min={0} 
+                    max={100} 
+                    className="w-14 md:w-28" 
+                    value={volume}
+                    onChange={(e) => setVolume(Number(e.value))}
+                />
+                <VolumeUpIcon onClick={() => volume < 100 && setVolume(volume + 10)} className="button" />
             </div>
         </div>
     )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { SessionProvider } from 'next-auth/react'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
